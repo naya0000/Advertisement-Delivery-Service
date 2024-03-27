ee554297b4822a9382ec557c59db1cba017ec932
naya0000 <naya60135@gmail.com>
2024-03-27 23:17:34 +0800

    add transaction

--- migrations/2_create_condition_table.up.sql
+++ migrations/2_create_condition_table.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS Condition (
     id SERIAL PRIMARY KEY,
-    advertisement_id INT NOT NULL,
+    ad_id INT NOT NULL,
     age_start INT,
     age_end INT,
-    gender VARCHAR(1), -- M, F
-    country VARCHAR(255), 
-    platform VARCHAR(255),
-    FOREIGN KEY (advertisement_id) REFERENCES Advertisement(id)
+    gender VARCHAR(1)[] DEFAULT NULL, -- ["M", "F"]
+    country VARCHAR(255)[] DEFAULT NULL, 
+    platform VARCHAR(255)[] DEFAULT NULL,
+    FOREIGN KEY (ad_id) REFERENCES Advertisement(id)
 );
--- pkg/api/api.go
+++ pkg/api/api.go
@@ -22,6 +22,7 @@ type Advertisement struct {
 
 // Conditions represents conditions for displaying an advertisement
 type Conditions struct {
+	AdID     uint8    `json:"ad_id"`
 	AgeStart *uint8   `json:"ageStart,omitempty"`
 	AgeEnd   *uint8   `json:"ageEnd,omitempty"`
 	Gender   *string  `json:"gender,omitempty"`
@@ -31,28 +32,49 @@ type Conditions struct {
 
 // CreateAdvertisement creates a new advertisement in the database
 func CreateAdvertisement(db *pg.DB, ad *Advertisement) error {
+	// Create a helper function for preparing failure results.
 
-	sqlStatement := `
-		INSERT INTO advertisement (title, start_at, end_at, conditions)
-		VALUES (?, ?, ?, ?)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	tx, err := db.BeginContext(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %v", err)
+	}
+	// Defer a rollback in case anything fails.
+	defer tx.Rollback()
+
+	sqlAdInsert := `
+		INSERT INTO advertisement (title, start_at, end_at)
+		VALUES (?, ?, ?)
 		RETURNING id
 	`
 	// Convert Conditions to JSON string
-	conditionsJSON, err := json.Marshal(ad.Conditions)
-	if err != nil {
-		return fmt.Errorf("failed to marshal conditions: %v", err)
-	}
+	// conditionsJSON, err := json.Marshal(ad.Conditions)
+	// if err != nil {
+	// 	return fmt.Errorf("failed to marshal conditions: %v", err)
+	// }
 
 	// Execute the SQL statement
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// defer cancel()
 
 	// var id int
-	_, err = db.ExecContext(ctx, sqlStatement, ad.Title, ad.StartAt, ad.EndAt)
+	// var result
+	result, err := tx.ExecContext(ctx, sqlAdInsert, ad.Title, ad.StartAt, ad.EndAt)
 	if err != nil {
-		return fmt.Errorf("failed to create advertisement: %v", err)
+		return fmt.Errorf("failed to insert advertisement: %v", err)
+	}
+	// Get the ID of the newly inserted advertisement
+	result
+	err = tx.QueryRow("SELECT LAST_INSERT_ID()").Scan(&adID)
+	if err != nil {
+		return fmt.Errorf("failed to get last insert ID: %v", err)
 	}
 
+	err = tx.Commit()
+	if err != nil {
+		return fmt.Errorf("failed to commit transaction: %v", err)
+	}
 	fmt.Println("Advertisement created successfully")
 	return nil
 }
